8912cde827767f4ef32ffbfa43884160aa9174d1
tabletseeker <148603424+tabletseeker@users.noreply.github.com>
2025-05-07 17:58:02 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+REPO=("DNSCrypt/dnscrypt-proxy")
+
+for i in ${REPO[@]}; do
+
+	GIT_URL="https://github.com/${i}/releases/latest"
+	VERSION+=($(curl -Ls -o /dev/null -w %{url_effective} ${GIT_URL} | sed -e 's@.*/@@' | tr -d v))
+
+done
+
+sudo docker build -t tabletseeker/whonix-builder:latest --build-arg DNSCRYPT_VER=${VERSION[0]} .
