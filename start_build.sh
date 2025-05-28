2c83b4cb63d14262c843a578aa1b67cfa0ff115f
tabletseeker <148603424+tabletseeker@users.noreply.github.com>
2025-05-28 12:42:23 +0000

    Fixing lo_clean function

--- start_build.sh
+++ start_build.sh
@@ -10,12 +10,10 @@ GIT_URL="https://github.com/Whonix/derivative-maker.git"
 read -a FLAVOR <<< "$FLAVOR"
 ### functions ###
 timestamp() { echo -e "\n${1} Time: $(date +'%D|%H:%M:%S')\n" >> ${2}; }
-lo_check() { LOOP_DEV=$(sudo losetup -nl | grep -E "(Whonix|Kicksecure).*" | awk '{print $1}'); \
-[ -z "${LOOP_DEV}" ] || { LOOP_PART=$(echo "${LOOP_DEV}" | sed -e 's,.*/,,'); \
-LOOP_DM=$(sudo dmsetup info -c -o name --noheadings | grep "${LOOP_PART}") || true; \
-sudo losetup -d ${LOOP_DEV}; [ -z "${LOOP_DM}" ] || sudo dmsetup remove -f ${LOOP_DM}; }; }
+lo_clean() { LOOP_DEV=$(sudo losetup -nl | grep -E "(Whonix|Kicksecure).*" | awk '{print $1}'); \
+[ -z "${LOOP_DEV}" ] || sudo losetup -d ${LOOP_DEV}; }
 build_cmd() { for ((i=0;i<${1};i++)); do timestamp 'Build Start' ${2}; \
-lo_check; \
+lo_clean; \
 /home/user/${TAG}/derivative-maker \
 --flavor ${FLAVOR[i]} \
 --target ${TARGET} \
@@ -23,7 +21,7 @@ lo_check; \
 --type ${TYPE} \
 --connection ${CONNECTION} \
 --repo ${REPO} \
-${OPTS}; timestamp 'Build End' ${2}; lo_check; done; }
+${OPTS}; timestamp 'Build End' ${2}; done; }
 ### get derivative key ###
 [ -f ~/derivative.asc ] || { wget https://www.whonix.org/keys/derivative.asc -O ~/derivative.asc; \
 gpg --keyid-format long --import --import-options show-only --with-fingerprint ~/derivative.asc; \
